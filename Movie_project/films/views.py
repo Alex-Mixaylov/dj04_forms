ba773d103f3ec3627584887f9bb1f99109d4a73a
Alex <guardamar81@gmail.com>
2024-08-15 19:45:56 +0400

    update views.py
    update views.py

--- Movie_project/films/views.py
+++ Movie_project/films/views.py
@@ -1,4 +1,21 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Movie
+from .forms import MovieForm
 
 def index(request):
-    return HttpResponse("<h1>INDEX.HTML</h1>")
+    return render(request, 'films/index.html')
+
+def add_movie(request):
+    if request.method == 'POST':
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('films')
+    else:
+        form = MovieForm()
+    return render(request, 'films/forms.html', {'form': form})
+
+def view_movies(request):
+    movies = Movie.objects.all()
+    return render(request, 'films/films.html', {'movies': movies})
+
