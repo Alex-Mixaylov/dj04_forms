dfcf8dfb5feba35f5a0fdc76029415c6ca678b67
Alex <guardamar81@gmail.com>
2024-08-15 21:22:43 +0400

    admin.py
    admin.py

--- Movie_project/films/admin.py
+++ Movie_project/films/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Movie
 
-# Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date', 'review', 'poster_preview')
+    search_fields = ('title', 'description')
+    list_filter = ('release_date',)
+
+    def poster_preview(self, obj):
+        if obj.poster:
+            return format_html('<img src="{}" width="100" />', obj.poster.url)
+        return "Нет изображения"
+    poster_preview.short_description = "Превью постера"
+
+    # Убедитесь, что поле poster доступно для редактирования
+    fields = ('title', 'poster', 'description', 'release_date', 'review')
+
+admin.site.register(Movie, MovieAdmin)
