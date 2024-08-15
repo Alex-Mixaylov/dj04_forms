166a3030b3517039c87ca5a15bdec7eea9d149eb
Alex <guardamar81@gmail.com>
2024-08-15 20:53:12 +0400

    update models.py
    update models.py

--- Movie_project/films/models.py
+++ Movie_project/films/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
+    poster = models.ImageField(upload_to='posters/', blank=True, null=True)
     description = models.TextField()
     release_date = models.DateField()
     review = models.TextField()
