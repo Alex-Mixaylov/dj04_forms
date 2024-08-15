0c54897908f23e47746b1a25449236ca485db105
Alex <guardamar81@gmail.com>
2024-08-15 19:47:33 +0400

    update urls.py
    update urls.py

--- Movie_project/films/urls.py
+++ Movie_project/films/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('add/', views.add_movie, name='add_movie'),
+    path('films/', views.view_movies, name='films'),
 ]
