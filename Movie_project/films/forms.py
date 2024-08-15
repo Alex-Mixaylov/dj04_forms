b29dfbbe42b38a2d99439c08e2561bb8a6b0c47b
Alex <guardamar81@gmail.com>
2024-08-15 20:34:22 +0400

    update forms.py
    update forms.py

--- Movie_project/films/forms.py
+++ Movie_project/films/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Movie
 
 class MovieForm(forms.ModelForm):
+    release_date = forms.DateField(
+        widget=forms.DateInput(
+            attrs={'type': 'date', 'class': 'form-control'}
+        )
+    )
+
     class Meta:
         model = Movie
         fields = ['title', 'description', 'release_date', 'review']
