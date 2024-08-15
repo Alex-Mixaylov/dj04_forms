32660fe84e573271fbf1c40ca1680fffa633076c
Alex <guardamar81@gmail.com>
2024-08-15 20:54:12 +0400

    update forms.py
    update forms.py

--- Movie_project/films/forms.py
+++ Movie_project/films/forms.py
@@ -4,10 +4,17 @@ from .models import Movie
 class MovieForm(forms.ModelForm):
     release_date = forms.DateField(
         widget=forms.DateInput(
-            attrs={'type': 'date', 'class': 'form-control'}
-        )
+            attrs={'type': 'date', 'class': 'form-control', 'format': '%d.%m.%Y'}
+        ),
+        input_formats=['%d.%m.%Y']
     )
 
     class Meta:
         model = Movie
-        fields = ['title', 'description', 'release_date', 'review']
+        fields = ['title', 'poster', 'description', 'release_date', 'review']
+        widgets = {
+            'poster': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'review': forms.Textarea(attrs={'class': 'form-control'}),
+        }
